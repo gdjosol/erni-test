31415140d5470f5b592de6b4ea0aa4a73a22951b
dioo <gdjosol@gmail.com>
2021-03-23 14:01:17 +0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
+import { useEffect, useState } from 'react';
+
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
+
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [entrances, setEntrances] = useState(3);
+
+  const [smallParking, setSmallParking] = useState(0);
+  const [mediumParking, setMediumParking] = useState(0);
+  const [largeParking, setLargeParking] = useState(0);
+
+  const [flatRate, setFlatRate] = useState(40);
+  const [flatRateHour, setFlatRateHour] = useState(3);
+
+  const [dayRate, setDayRate] = useState(5000);
+  const [dayHours, setDayHours] = useState(24);
+
+  const [smallParkingFee, setSmallParkingFee] = useState(20);
+  const [mediumParkingFee, setMediumParkingFee] = useState(60);
+  const [largeParkingFee, setLargeParkingFee] = useState(100);
+
+  const [parkedCars, setParkedCars] = useState([]);
+
+  useEffect(() => {
+    
+  });
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Grid container className="main-content">
+        <Grid item md={2} >
+          <Paper className="testg">
+            hello world
+            </Paper>
+        </Grid>
+      </Grid>
     </div>
   );
 }
